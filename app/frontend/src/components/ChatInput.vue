3ddaed372be503efd300ce987aef00b2bcc80b23
ges0909 <gerrit.schrader@gmail.com>
2026-05-15 18:03:58 +0200

    style: increase history icon size from h-5/w-5 to h-6/w-6

--- app/frontend/src/components/ChatInput.vue
+++ app/frontend/src/components/ChatInput.vue
@@ -99,7 +99,7 @@ const hasHistory = computed(() => history.value.length > 0);
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            class="h-5 w-5"
+            class="h-6 w-6"
             viewBox="0 0 20 20"
             fill="currentColor"
           >
